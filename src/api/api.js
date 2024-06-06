626fa20e586169be1b97cfc970cbaca858ef56b2
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-06-06 21:22:53 +0000

    API implementada na página de `Create` & Criação do `<form>`

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,35 @@
 export const Api = {
     baseUrl: 'https://desenvolvimento-integrado-de-backend-gtnt.onrender.com/',
-
     inventario: {
-        endpoint: function() {
-            return Api.baseUrl + 'inventario'
-        },
-        readAll: function() {
-            return this.endpoint() + '/'
-        }
+      endpoint: function () {
+        return Api.baseUrl + 'inventario'
+      },
+      readAll: function () {
+        return this.endpoint() + '/'
+      },
+      create: function () {
+        return this.endpoint() + '/'
+      }
     },
-
+  
     buildApiGetRequest: function (url) {
-        return fetch(url, { method: 'GET' }).catch(function (error) {
-            console.error('Erro ao carregar dados: ' + url, error)
-            toast.error('Erro ao carregar dados.')
-          })
+      return fetch(url, { method: 'GET' }).catch(function (error) {
+        console.error('Erro ao carregar dados: ' + url, error)
+        toast.error('Erro ao carregar dados.')
+      })
+    },
+  
+    buildApiPostRequest: function (url, body) {
+      return fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+        .catch(function (error) {
+          console.error('Erro ao enviar dados: ' + url, error)
+          toast.error('Erro ao enviar dados.')
+        })
     }
-}
+  }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,3 +1,54 @@
+import { toast } from "react-toastify"
+import { Api } from "../api/api"
+import { useNavigate } from "react-router-dom"
+
 export default function Create() {
-    return <div>Create</div>
+  const navigate = useNavigate()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const pricelists = {
+      nome: event.target.nome.value,
+      imagem: event.target.imagem.value,
+      Preço: event.target.Preço.value
+    }
+
+    const apiUrl = Api.inventario.create()
+
+    const response = await Api.buildApiPostRequest(apiUrl, pricelists)
+
+    if (response.ok) {
+      toast.success('Criado com sucesso!')
+      navigate('/')
+    } else {
+      const body = await response.json()
+      toast.error('Erro ao criar: ' + body.error)
+    }
   }
+
+  return (
+    <div>
+      <h1>Criar Item</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nome">Nome*:</label><br />
+          <input type="text" id="nome" name="nome" placeholder="Insira o nome" />
+        </div>
+        <div>
+          <label htmlFor="imagem">Imagem (URL)*:</label><br />
+          <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem" />
+        </div>
+        <div>
+          <label htmlFor="Preço">Preço (opcional):</label><br />
+          <input type="text" id="Preço" name="Preço" placeholder="Insira o preço" />
+        </div>
+        <div>
+          <button type="submit">Criar</button>
+          <button type="reset">Reiniciar</button>
+        </div>
+      </form>
+    </div>
+  )
+}
